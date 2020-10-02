056a9ccfc87227d8bf6388f73efc0a9f17f62b54
jobnow <ricardogomesweb@gmail.com>
2020-10-02 08:18:59 -0300

    s3 images

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -31,7 +31,7 @@ export default {
   multer: {
     storage: multer.diskStorage({
       destination: tmpFolder,
-      filename(request, file, callback) {
+      filename: (request, file, callback) => {
         const fileHash = crypto.randomBytes(10).toString('HEX');
         const fileName = `${fileHash}-${file.originalname}`;
 
@@ -43,7 +43,7 @@ export default {
   config: {
     disk: {},
     aws: {
-      bucket: 'app.bankpague.com',
+      bucket: 'bank-imagens',
     },
   },
 } as IUploadConfig;

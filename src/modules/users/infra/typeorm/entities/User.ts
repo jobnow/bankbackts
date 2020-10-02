9e3e587dc5de5f17731f7dc2d22db37f5520e049
jobnow <ricardogomesweb@gmail.com>
2020-10-02 01:19:44 -0300

    temp

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -41,7 +41,7 @@ class User {
 
     switch (uploadConfig.driver) {
       case 'disk':
-        return `${process.env.APP_WEB_URL}/tmp/uploads/avatar/${this.avatar}`;
+        return `${process.env.APP_WEB_URL}/uploads/${this.avatar}`;
       case 's3':
         return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
       default:

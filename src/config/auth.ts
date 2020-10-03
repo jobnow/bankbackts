69be34bd277979e567c5b308cf824709ed39bfb4
jobnow <ricardogomesweb@gmail.com>
2020-10-03 11:08:22 -0300

    10s

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
     secret: process.env.APP_SECRET || 'default_secret',
-    expiresIn: process.env.APP_EXPIRES || '1m',
+    expiresIn: process.env.APP_EXPIRES || '10s',
   },
 };

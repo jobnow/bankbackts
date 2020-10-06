8b4157678b877e0afc66a1a80cdc7a7afdcfd9af
jobnow <ricardogomesweb@gmail.com>
2020-10-06 17:08:27 -0300

    init

--- dist/src/config/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        secret: process.env.APP_SECRET || 'default_secret',
-        expiresIn: process.env.APP_EXPIRES || '1d',
-    },
-};
